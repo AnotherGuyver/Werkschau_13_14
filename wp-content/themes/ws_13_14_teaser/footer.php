b655649b8fe61d71487e3d54f1b112db0f81620e
anotherguyver <neptunslawa@googlemail.com>
2013-11-11 22:11:28 +0100

    Logo Placeholder
    - Logo Platzhalter eingebaut
    - Einige CSS fixes

--- wp-content/themes/ws_13_14_teaser/footer.php
+++ wp-content/themes/ws_13_14_teaser/footer.php
@@ -1,7 +1,7 @@
 	<li id="cn-newsletter-btn" class="layer" data-depth="0">
 		<a href="javascript:" id="show-newsletter-btn">BLEIB AUF DEM LAUFENDEN!</a>
 	</li>
-	<li id="cn-ol-newsletter" class="layer" data-depth="0.1">
+	<li id="cn-ol-newsletter" class="layer" data-depth="0.03">
 		<div id="in-ol-newsletter">
 			<div id="in-cn-newsletter">
 				<a href="javascript:" id="close-nesletter-btn"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/close.svg" alt="" id="close-newsletter-btn"></a>
@@ -12,6 +12,9 @@
 				<?php echo yksemeProcessSnippet('0a387e60d3'); ?>	
 			</div>
 		</div>
+	</li>
+	<li class="layer logo" data-depth="0.01">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="WERKSCHAU" id="logo">
 	</li>
 
 	</ul>
