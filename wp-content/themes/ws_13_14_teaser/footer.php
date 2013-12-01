8ca5084531d596a83d9b57b57e7dfe65539ad9db
anotherguyver <neptunslawa@googlemail.com>
2013-12-02 00:37:30 +0100

    CSS Changes
    Kleine CSS Veränderungen

--- wp-content/themes/ws_13_14_teaser/footer.php
+++ wp-content/themes/ws_13_14_teaser/footer.php
@@ -1,17 +1,3 @@
-
-	<div id="cn-ol-newsletter" class="layer" data-depth="0.03">
-		<div id="in-ol-newsletter">
-			<div id="in-cn-newsletter">
-				<a href="javascript:" id="close-nesletter-btn"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/close.svg" alt="" id="close-newsletter-btn"></a>
-				<div id="newsletter-title">NEWSLETTER ANMELDUNG</div>
-				<div id="newsletter-text">
-					Hier erfährst du alle Neuigkeiten zur Werkschau! Von unseren neuesten Fortschritten bis zu den aktuellen Terminen. Deine Daten werden nicht an dritte weitergegeben - versprochen ;)
-				</div>
-				<?php echo yksemeProcessSnippet('0a387e60d3'); ?>	
-			</div>
-		</div>
-	</div>
-
 </div>
 
 <script>
--- wp-content/themes/ws_13_14_teaser/index.php
+++ wp-content/themes/ws_13_14_teaser/index.php
@@ -80,6 +80,23 @@
 				</div>
 			</div>
 		</div>
+		<div id="newsletter" class="square not-clickable expand">
+			<div class="info-inner">
+				<!--<a href="javascript:" id="close-nesletter-btn"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/close.svg" alt="" id="close-newsletter-btn"></a>-->
+				<div class="info-content">
+					<div class="top-message">
+						BLEIB AUF DEM<br />LAUFENDEN
+					</div>
+					<div class="mail-field">
+						<?php echo yksemeProcessSnippet('0a387e60d3'); ?>	
+					</div>
+				</div>
+				<!--<div id="newsletter-text">
+					Hier erfährst du alle Neuigkeiten zur Werkschau! Von unseren neuesten Fortschritten bis zu den aktuellen Terminen. Deine Daten werden nicht an dritte weitergegeben - versprochen ;)
+				</div>-->
+
+			</div>
+		</div>
 
 	</div>
 </div>
