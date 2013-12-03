d768cbb10e076e719feb8132af5bc5640585b709
anotherguyver <neptunslawa@googlemail.com>
2013-12-03 23:39:20 +0100

    Achievement Sorting
    - Fehler, bei dem alle Countdowns verschwinden, wenn es keine Achievements gibt, wurde behoben.

--- wp-content/themes/ws_13_14_teaser/index.php
+++ wp-content/themes/ws_13_14_teaser/index.php
@@ -9,32 +9,6 @@
 				</div>
 			</div>
 		</div>
-		<?php
-		global $post;
-		$my_query = getByType('achievement');
-		if( $my_query->have_posts() ) {
-			while ($my_query->have_posts()) : $my_query->the_post(); ?>
-		  	<!-- TEMPLATE FOR TRIANGLE GOES HERE -->
-				<div class="square  <?php echo custom_taxonomies_terms_links();?> <?php echo types_render_field("groesse",array('raw'=>true)); ?>">
-					<?php the_post_thumbnail('square-thumb'); ?>
-
-					<div class="category">
-						<div class="circle-wrapper">
-							<div class="circle <?php echo custom_taxonomies_terms_links(); ?>">
-								<?php echo custom_taxonomies_terms_links(); ?>	
-							</div>
-						</div>
-					</div>
-					<div class="message <?php echo custom_taxonomies_terms_links(); ?>">
-						<?php echo strip_tags(get_the_title()); ?>
-					</div>
-				</div>
-		  	<!-- TEMPLATE FOR TRIANGLE ENDS HERE -->
-		  					
-		<?php
-			endwhile; 	
-		}
-		?>
 		<div class="square not-clickable countdown seconds">
 			<div class="info-inner">
 				<div class="info-content">
@@ -92,6 +66,33 @@
 				</div>
 			</div>
 		</div>
+		<?php
+		global $post;
+		$my_query = getByType('achievement');
+		if( $my_query->have_posts() ) {
+			while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		  	<!-- TEMPLATE FOR TRIANGLE GOES HERE -->
+				<div class="square  <?php echo custom_taxonomies_terms_links();?> <?php echo types_render_field("groesse",array('raw'=>true)); ?>">
+					<?php the_post_thumbnail('square-thumb'); ?>
+
+					<div class="category">
+						<div class="circle-wrapper">
+							<div class="circle <?php echo custom_taxonomies_terms_links(); ?>">
+								<?php echo custom_taxonomies_terms_links(); ?>	
+							</div>
+						</div>
+					</div>
+					<div class="message <?php echo custom_taxonomies_terms_links(); ?>">
+						<?php echo strip_tags(get_the_title()); ?>
+					</div>
+				</div>
+		  	<!-- TEMPLATE FOR TRIANGLE ENDS HERE -->
+		  					
+		<?php
+			endwhile; 	
+		}
+		?>
+		
 
 	</div>
 </div>
