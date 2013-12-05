fa590995434a374811bea6319984a12c0b08b8f1
anotherguyver <neptunslawa@googlemail.com>
2013-12-05 09:48:41 +0100

    Meta Viewport
    Viewport Meta-Tag für Mobile hinzugefügt

--- wp-content/themes/ws_13_14_teaser/header.php
+++ wp-content/themes/ws_13_14_teaser/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<title>Werkschau WS13/14</title>
 	<?php wp_deregister_script('jquery'); // for compatibility reasons, mailchimp plugin messes with embedded jquery?> 
 	<?php wp_head(); ?>
