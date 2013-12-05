1526f7b7c37f912bc9824e28905f1d0369aa62ee
anotherguyver <neptunslawa@googlemail.com>
2013-12-05 13:08:01 +0100

    Black Square
    Schwazes Viereck hinzugefügt.

--- wp-content/themes/ws_13_14_teaser/header.php
+++ wp-content/themes/ws_13_14_teaser/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-	<title>Werkschau WS13/14</title>
+	<title>Werkschau WS13/14 jj</title>
 	<?php wp_deregister_script('jquery'); // for compatibility reasons, mailchimp plugin messes with embedded jquery?> 
 	<?php wp_head(); ?>
 </head>
