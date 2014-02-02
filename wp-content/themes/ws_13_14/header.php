60d2c6faf1292b2fcd3a62f7a329ebbdfe5abe1a
anotherguyver <neptunslawa@googlemail.com>
2014-02-02 19:39:54 +0100

    Finished Map
    Anfahrt Section fertig.

---
+++ wp-content/themes/ws_13_14/header.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>sds</title>
+	<?php wp_head(); ?>
+</head>
+<body>
+	<div id="navi-outer" class="fullwidth">
+		<div id="navi-inner">
+			<ul id="navi-main" class="large-block-grid-9">
+				<li class="navi-element"><a href="" class="navi-link whitelink">
+						<div class="link-content">ABSOLVENTEN</div>
+					</a></li>
+				<li class="navi-element"><a href="" class="navi-link whitelink">
+						<div class="link-content">ANFAHRT</div>
+					</a></li>
+				<li class="navi-element"><a href="" class="navi-link whitelink">
+						<div class="link-content">WERKSCHAU</div>
+					</a></li>
+			</ul>
+		</div>
+	</div>
+	<ul id="wrapper">
--- wp-content/themes/ws_13_14/index.php
+++ wp-content/themes/ws_13_14/index.php
@@ -1,79 +1,67 @@
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>sds</title>
-	<?php wp_head(); ?>
-</head>
-<body>
-	<div id="navi-outer" class="fullwidth">
-		<div id="navi-inner">
-			<ul id="navi-main" class="large-block-grid-9">
-				<li class="navi-element"><a href="" class="navi-link whitelink">
-						<div class="link-content">ABSOLVENTEN</div>
-					</a></li>
-				<li class="navi-element"><a href="" class="navi-link whitelink">
-						<div class="link-content">ANFAHRT</div>
-					</a></li>
-				<li class="navi-element"><a href="" class="navi-link whitelink">
-						<div class="link-content">WERKSCHAU</div>
-					</a></li>
-			</ul>
+<?php get_header(); ?>
+<li id="anfahrt-section" class="section fullscreen">
+	<div id="anfahrt-info-outer">
+		<div id="anfahrt-info-inner">
+			<div id="anfahrt-info-content">
+				<p>
+				Hochschule Augsburg<br />
+				Fakultät für<br />
+				Gestaltung<br />
+				</p>
+				<p>
+				Friedberger Straße 2<br />
+				(Über Schülestraße)<br />
+				86161 Augsburg<br />
+				</p>
+				<p>
+				Tel: +49 (0)821 5586-3401<br />
+				EMAIL : werkschau@hs-augsburg.de<br />
+				</p>
+				<p>
+				Haltestelle – Fachhochschule<br />
+				Linie 6, 32, 33, 35, 66, 93<br />
+				</p>
+			</div>
 		</div>
 	</div>
-	<ul id="wrapper">
-		<li id="anfahrt-section" class="section fullscreen">
-			<div id="anfahrt-info-outer">
-				<div id="anfahrt-info-inner">
-					<div id="anfahrt-info-content">
-						ss
-					</div>
-				</div>
-			</div>
-			<script type="text/javascript" 
-				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhEUtEAOUSlf4mlbFtaNvCUT-qAV00v-w&sensor=true">
-	   	</script>
-	   	<script type="text/javascript">
-	      function initialize() {
-	      	var mapOptions = {
-	         	center: new google.maps.LatLng(-34.397, 150.644),
-	         	zoom: 8
-	       	};
-	        	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-		   }
-		   google.maps.event.addDomListener(window, 'load', initialize);
-		   </script>
-	   	<div id="map" />
-		</li>
-		<li id="main-section" class="section fullscreen">ss</li>	
-		<li id="werkschau-section" class="section fullscreen">ss</li>
-	
-		<li id="footer" class="fullwidth">
-			<div id="footer-inner" class="row">
-				<div id="sponsors" class="large-9">
-					<?php 
-						$sponsoren = new WP_Query( array('post_type'=>'sponsoren') );
-						while ($sponsoren->have_posts()) : $sponsoren->the_post();
-							echo '<a>sds</a>';
-						endwhile;
-					?>
-				</div>
-				<div id="footer-links" class="push-8 large-1">
-					<ul class="footer-links-container large-block-grid-2">
-						<li>
-							<a href="" class="footer-link whitelink">
-								<div class="footer-link-content">KONTAKT</div>
-							</a>
-						</li>
-						<li>
-							<a href="" class="footer-link whitelink">
-								<div class="footer-link-content">IMPRESSUM</div>
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</li>
-	</ul>
-</body>
-</html>
+	<script type="text/javascript" 
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhEUtEAOUSlf4mlbFtaNvCUT-qAV00v-w&sensor=true">
+	</script>
+	<script type="text/javascript">
+   function initialize() {
+   	var myLatlng = new google.maps.LatLng(48.3586518,10.906289);
+   	var mapOptions = {
+      	center: myLatlng,
+      	zoom: 15,
+      	disableDefaultUI: true,
+    	};
+
+    	var styles = [
+		  {
+		    "stylers": [
+		      { "saturation": -100 }
+		    ]
+		  }
+		];
+
+    	var styledMap = new google.maps.StyledMapType(styles, {name: "Styled Map"});
+
+     	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+     	map.mapTypes.set('map_style', styledMap);
+		map.setMapTypeId('map_style');
+
+		var marker = new google.maps.Marker({
+	      position: myLatlng,
+	      map: map,
+	      title: 'Hello World!',
+	      icon: '<?php echo get_template_directory_uri() ?>/img/flash.png'
+	  });
+   }
+   google.maps.event.addDomListener(window, 'load', initialize);
+   </script>
+	<div id="map" />
+</li>
+<li id="main-section" class="section fullscreen">ss</li>	
+<li id="werkschau-section" class="section fullscreen">ss</li>
+<?php get_footer(); ?>
